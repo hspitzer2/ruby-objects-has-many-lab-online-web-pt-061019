cd3fd3b1772fea76f1dd98a406b44c9c8314a17a
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-17 22:36:05 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,30 +1,5 @@
 
 class Post
-  attr_accessor :artist, :name
-
-   @@all = []
-
-  def initialize (name)
-    @name = name
-    artist = artist
-    @@all << self
-  end
-
-  def artist_name
-    if artist
-      self.artist.name
-    else
-      nil
-    end
-  end
-
-    def self.all
-      @@all
-    end
-
-end
-
-class Post
 
    @@all = []
   attr_accessor  :author, :title

4c4cd39b07b056c1c7337379ca5b57f4c28180d9
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-17 21:46:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,6 +8,10 @@ class Artist
     @name = name
     @songs = []
   end
+  
+  def songs
+    @songs
+  end
 
   def add_song(songs)
     self.songs << songs
@@ -22,9 +26,7 @@ class Artist
     @@song_count += 1
   end
 
-  def songs
-    @songs
-  end
+  
 
   def self.song_count
     @@song_count

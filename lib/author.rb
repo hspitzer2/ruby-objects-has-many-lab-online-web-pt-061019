032ec9d3f3631f9efb568e250410340ccfee1f9d
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-17 22:21:31 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,27 +1,27 @@
 class Author
   attr_accessor :name
-    @@post_count = 0  #set counter at 0
+    @@post_count = 0  
 
     def initialize(name)
       @name = name
       @posts = []
     end
 
-   def posts # this is a custom getter method to use [] can change in future
+   def posts 
      @posts
    end
 
    def add_post(post)
      @posts << post
-     post.author = self #attached/opens title to author
-     @@post_count += 1 #adds all new
+     post.author = self
+     @@post_count += 1 
    end
 
    def add_post_by_title(post_title)
-     post = Post.new(post_title) #attached/opens title to post
+     post = Post.new(post_title) 
      @posts << post
-     post.author = self #accesses author info
-     @@post_count += 1 # need for collecting by title
+     post.author = self 
+     @@post_count += 1 
    end
 
     def self.post_count
